28a37005c2ccc68d1dba49a86587cc0a6019e1a1
Luis Sandoval <luismsandoval96@gmail.com>
2022-12-10 11:10:58 -0800

    useOptions hook implemented, options context provider

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,14 +1,11 @@
-import React, { useContext, useState } from "react";
-import { UserOptionsContext } from "../../context/OptionsContext";
+import React, { useContext } from "react";
 import { ScreenContext } from "../../context/ScreenContext";
+import { useOptions } from "../../hooks/useOptions";
 import "./Title.css";
 
 function Title() {
   const { setScreen } = useContext(ScreenContext);
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleModal = () => {
-    setIsOpen(!isOpen);
-  };
+  const { Options, isOpen, toggle } = useOptions(false);
 
   return (
     <div
@@ -31,81 +28,11 @@ function Title() {
         }}
       >
         <button onClick={() => setScreen!(1)}>Start Game</button>
-        <button onClick={toggleModal}>Options</button>
+        <button onClick={toggle}>Options</button>
         {/* <button onClick={() => electron.app.quit()}>Quit</button> */}
         <button onClick={() => window.close()}>Quit</button>
       </div>
-      {isOpen && <Options toggleModal={toggleModal} />}
-    </div>
-  );
-}
-
-type OptionsProps = {
-  toggleModal: () => void;
-};
-
-function Options(props: OptionsProps) {
-  const { userOptions, setUserOptions } = useContext(UserOptionsContext);
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-    setUserOptions({ ...userOptions, [name]: value });
-    console.log(name, value, userOptions);
-  };
-
-  return (
-    <div>
-      <h1>Options</h1>
-      <form style={{ display: "flex", flexDirection: "column" }}>
-        <label>
-          <input
-            name="music"
-            type="radio"
-            value="true"
-            checked={userOptions.music === true}
-            onChange={handleChange}
-          />
-          Music on
-          <input
-            name="music"
-            type="radio"
-            value="false"
-            checked={userOptions.music === false}
-            onChange={handleChange}
-          />
-          Music off
-        </label>
-        <label>
-          <input
-            name="soundfx"
-            type="radio"
-            value="true"
-            checked={userOptions.soundfx === true}
-            onChange={handleChange}
-          />
-          Soundfx on
-          <input
-            name="soundfx"
-            type="radio"
-            value="false"
-            checked={userOptions.soundfx === false}
-            onChange={handleChange}
-          />
-          Soundfx off
-        </label>
-        <label>
-          <input
-            name="volume"
-            type="range"
-            min="0"
-            max="100"
-            value={userOptions.volume}
-            onChange={handleChange}
-          />
-          Volume
-        </label>
-      </form>
-      <button onClick={props.toggleModal}>Close</button>
+      {isOpen && <Options/>}
     </div>
   );
 }
